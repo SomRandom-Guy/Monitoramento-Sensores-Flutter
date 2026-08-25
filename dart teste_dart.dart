b8fbde8795be6d2ec2ec5394a4890f08d8c2327d
gabriel <gabriel.dge.assis@gmail.com>
2026-08-25 11:44:49 -0300

    inicia app Flutter VerdeSmart com dashboard e AreaCard

---
+++ dart teste_dart.dart
@@ -0,0 +1,10 @@
+void main(){
+    print('VerdeSmart Flutter');
+}
+void main(){
+    String rodovia = 'SP=280' ; //String = testo em dart
+    int idArea = 1;  //int = numero inteiro em dart
+    double densidade = 0.72; // double = numero decimal em dart
+    bool urgente = false ; //bool = bolean em dart
+    print('$rodovia | área #$idArea | densidade=$densidade | urgente=$urgente');
+}
